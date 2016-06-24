e28a94d7bc9502c2d98e3b380fbda8c62525c9a9
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-24 22:50:22 +0100

    more changes on frame

--- LogsExploration/Script.fsx
+++ LogsExploration/Script.fsx
@@ -28,7 +28,11 @@ let df =
           Level = s.GetAs<string>("Level")
           Source = s.GetAs<string>("Source") 
           Text = s.GetAs<string>("Text")
-          Instance = s.GetAs<string>("Instance") })
+          Instance = 
+            match s.GetAs<string>("Instance") with
+            | "bron" -> "Instance-1"
+            | "rct-live" -> "Instance-2"
+            | _ -> "Instance-3" })
     |> Frame.ofRecords
 
 type Data = {
@@ -38,9 +42,11 @@ type Data = {
 and Count = {
     Date: DateTime
     Count: int
-}
+    PartialLogs: Log list
+} with
+    static member Default date = { Date = date; Count = 0; PartialLogs = [] }
 
-let errors =
+let computeCount predicateOnRow =
     df
     |> Frame.pivotTable
         (fun _ c -> 
@@ -48,58 +54,48 @@ let errors =
             new DateTime(date.Year, date.Month, date.Day, date.Hour, 0, 0))
         (fun _ c -> c.GetAs<string>("Instance"))
         (fun f -> 
-            f 
-            |> Frame.filterRowValues (fun c -> c.GetAs<string>("Level") = "error")
-            |> Frame.getCols
-            |> Stats.count)
-    |> Frame.fillMissingWith 0
+            let x =
+                f |> Frame.filterRowValues predicateOnRow
+            x |> Frame.countRows, 
+            x 
+            |> Frame.rows
+            |> Series.observations 
+            |> Seq.map (fun (_, s) ->
+                { Date = s.GetAs<DateTime>("Date")
+                  Level = s.GetAs<string>("Level")
+                  Source = s.GetAs<string>("Source") 
+                  Text = s.GetAs<string>("Text")
+                  Instance = s.GetAs<string>("Instance") })
+            |> Seq.truncate 10
+            |> Seq.sortBy (fun e -> e.Date)
+            |> Seq.toList)
     |> Frame.sortRowsByKey
     |> Frame.getCols
     |> Series.observations
     |> Seq.map (fun (k, v) -> 
         { Instance = k
           Counts = 
-            v 
-            |> Series.observations 
-            |> Seq.map (fun (k, v) -> 
+            v
+            |> Series.map (fun k (count, logs) ->
                 { Date = k
-                  Count = v }) 
+                  Count = count
+                  PartialLogs = logs })
+            |> Series.fillMissingUsing Count.Default
+            |> Series.observations
+            |> Seq.map snd
             |> Seq.toList })
     |> Seq.toList
+    
 
-let mdRefreshes =
-    df
-    |> Frame.pivotTable
-        (fun _ c -> 
-            let date = c.GetAs<DateTime>("Date")
-            new DateTime(date.Year, date.Month, date.Day, date.Hour, 0, 0))
-        (fun _ c -> c.GetAs<string>("Instance"))
-        (fun f -> 
-            f 
-            |> Frame.filterRowValues (fun c -> c.GetAs<string>("Text").Contains("md-refresh"))
-            |> Frame.getCols
-            |> Stats.count)
-    |> Frame.fillMissingWith 0
-    |> Frame.sortRowsByKey
-    |> Frame.getCols
-    |> Series.observations
-    |> Seq.map (fun (k, v) -> 
-        { Instance = k
-          Counts = 
-            v 
-            |> Series.observations 
-            |> Seq.map (fun (k, v) -> 
-                { Date = k
-                  Count = v }) 
-            |> Seq.toList })
-    |> Seq.toList
+let errors =      computeCount (fun c -> c.GetAs<string>("Level") = "error")
+let mdRefreshes = computeCount (fun c -> c.GetAs<string>("Source") = "masterdata-rfsh")
+let compileFSX =  computeCount (fun c -> c.GetAs<string>("Text") = "Compiling FSX configuration...")
 
 #I __SOURCE_DIRECTORY__
 #r "../packages/Suave/lib/net40/Suave.dll"
 #r "../packages/Newtonsoft.Json/lib/net40/Newtonsoft.Json.dll"
 
 open Suave
-open Suave.Json
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
@@ -116,6 +112,7 @@ let JSON v =
 let app = 
     GET >=> choose
         [ path "/errors" >=> JSON errors
-          path "/mdrefreshes" >=> JSON mdRefreshes ]
+          path "/mdrefreshes" >=> JSON mdRefreshes
+          path "/compilefsx" >=> JSON compileFSX ]
 
 startWebServer defaultConfig app
